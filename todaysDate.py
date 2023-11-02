e3243b54a05fd1ca8c8920245431712ecd53a44c
zahrabateninia <zahrabatenin@gmail.com>
2023-11-02 17:29:06 +0330

    Use timedelta to see what date will be 7 days later

--- todaysDate.py
+++ todaysDate.py
@@ -4,3 +4,5 @@ todaysDate = datetime.date.today()
 print(todaysDate.day) # to just print today's day
 print(todaysDate.weekday())
 print(todaysDate.isoweekday())
+tDelta = datetime.timedelta(days=7)
+print(todaysDate + tDelta)
